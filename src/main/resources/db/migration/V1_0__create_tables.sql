fa1fbfa90c94949dee7344ae9c96b83832af8777
netorochaa <jrr.neto@hotmail.com>
2023-06-12 23:11:28 -0300

    populate usuario table

--- src/main/resources/db/migration/V1_0__create_tables.sql
+++ src/main/resources/db/migration/V1_0__create_tables.sql
@@ -11,26 +11,54 @@ create table API.CLIENTE (
    TELEFONE   VARCHAR2(100)
 );
 
-create table API.USUARIO (
-   ID_USUARIO BIGINT               default 0  not null,
-   NOME VARCHAR2(200) not null,
-   LOGIN VARCHAR2(200) not null,
-   SENHA VARCHAR2(200) not null,
-   EMAIL   VARCHAR2(100),
-   PERMISSOES  VARCHAR2(2000)
-);
-
 create sequence API.SQ_ID_CLIENTE minvalue 1 start with 1 increment by 1 nocache;
-create sequence API.SQ_ID_USUARIO minvalue 1 start with 1 increment by 1 nocache;
 
 alter table API.CLIENTE
    add constraint PK_CLIENTE primary key (ID_CLIENTE);
-
-alter table API.USUARIO
-   add constraint PK_USUARIO primary key (ID_USUARIO);
 
 INSERT INTO API.CLIENTE (ID_CLIENTE, NOME, ENDERECO, EMAIL, TELEFONE) VALUES (API.SQ_ID_CLIENTE.nextval,'José','Rua J, 345','jose14@outlook.com','53 988667788');
 INSERT INTO API.CLIENTE (ID_CLIENTE, NOME, ENDERECO, EMAIL, TELEFONE) VALUES (API.SQ_ID_CLIENTE.nextval,'Jão','Rua H, 343','jose14@outlook.com','53 988098432');
 INSERT INTO API.CLIENTE (ID_CLIENTE, NOME, ENDERECO, EMAIL, TELEFONE) VALUES (API.SQ_ID_CLIENTE.nextval,'Manolo','Rua M, 342','jose14@outlook.com','53 98098234');
 INSERT INTO API.CLIENTE (ID_CLIENTE, NOME, ENDERECO, EMAIL, TELEFONE) VALUES (API.SQ_ID_CLIENTE.nextval,'Wlisses','Rua N, 341','jose14@outlook.com','53 98098234');
 INSERT INTO API.CLIENTE (ID_CLIENTE, NOME, ENDERECO, EMAIL, TELEFONE) VALUES (API.SQ_ID_CLIENTE.nextval,'Leonardo','Rua O, 320','jose14@outlook.com','53 980928347');
+
+/*==============================================================*/
+/* Table: USUARIO                                               */
+/*==============================================================*/
+
+create table API.USUARIO (
+   ID_USUARIO BIGINT default 0 not null,
+   NOME VARCHAR2(200) not null,
+   LOGIN VARCHAR2(200) not null,
+   SENHA VARCHAR2(200) not null,
+   EMAIL VARCHAR2(100),
+   PERMISSOES VARCHAR2(2000)
+);
+
+create sequence API.SQ_ID_USUARIO minvalue 1 start with 1 increment by 1 nocache;
+
+alter table API.USUARIO
+   add constraint PK_USUARIO primary key (ID_USUARIO);
+
+INSERT INTO API.USUARIO (
+    ID_USUARIO,
+    NOME,
+    LOGIN,
+    SENHA,
+    EMAIL,
+    PERMISSOES
+) VALUES (
+    API.SQ_ID_USUARIO.nextval,
+    'Root',
+    'root',
+    '12345',
+    'root@email.com',
+    'LEITURA_USUARIO,ESCRITA_USUARIO'
+ ), (
+    API.SQ_ID_USUARIO.nextval,
+    'Usuário para exclusão',
+    'excluir',
+    '123',
+    'excluir@email.com',
+    'LEITURA_USUARIO,ESCRITA_USUARIO'
+ );
